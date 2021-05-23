db231208312815aa5eb901eab392dbe2371a75e6
John Doe <stanislav_2010@icloud.com>
2021-05-23 21:02:43 +0500

    dsads

--- JS/main.js
+++ JS/main.js
@@ -3,12 +3,26 @@ document.addEventListener('DOMContentLoaded', function() {
     let menu = document.querySelector('.burger-menu');
     let menuBody = document.querySelector('.header-menu');
 
+    function sliderMob(sliderName, sliderWrapper) {
+        sliderName.forEach(element => {
+            element.classList.add('swiper-slide');
+            document.querySelector(sliderWrapper).append(element);  
+        });
+    }
+
+    function menuItemRend(item) {
+        const menuItem = document.createElement('li');
+        menuItem.classList.add('header-menu__item');
+        menuItem.append(item);
+        return menuItem;
+    }
+
     if (menu != null) {
         menu.addEventListener('click', function() {
         let group = [menu, menuBody];
 
         group.forEach(element => {
-            element.classList.toggle('active')
+            element.classList.toggle('active');
         });
         
         document.body.classList.toggle('lock');
@@ -29,12 +43,6 @@ document.addEventListener('DOMContentLoaded', function() {
     ibg();
 
     if (document.body.clientWidth < 1001) {
-        function menuItemRend(item) {
-            const menuItem = document.createElement('li');
-            menuItem.classList.add('header-menu__item');
-            menuItem.append(item);
-            return menuItem;
-        }
         const menu = document.querySelector('.header-menu');
         const menuList = document.querySelector('.header-menu__list');
         const phone = document.querySelector('.header-contacts__phone');
@@ -51,27 +59,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const activeHideMenu = document.querySelectorAll('.spoiler');
     const hideMenu = document.querySelectorAll('.questions-item');
+    const questions = document.querySelector('.questions');
     
     for (let i = 0; i < activeHideMenu.length; i++) {
-        document.addEventListener('click', e => {
-            let target = e.target;
-            let its_menu = target == hideMenu[i] || hideMenu[i].contains(target);
-            let its_activeHideMenu = target == activeHideMenu[i];
-            let menu_is_active = hideMenu[i].classList.contains('active');
-            
-            if (!its_menu && !its_activeHideMenu && menu_is_active) {
-              toggleMenu();
-            }
-          });
-        const toggleMenu = () => {
+        activeHideMenu[i].addEventListener('click', (e)=> {
+            console.log(e.target);
             hideMenu[i].classList.toggle('active');
-            hideMenu[i].classList.toggle('disActive');
-        };
-
-        activeHideMenu[i].addEventListener('click', ()=> {
-            toggleMenu();
         });
         
+        questions.addEventListener('click', (e)=> {
+            if (e.target != activeHideMenu[i] && e.target != hideMenu[i] && e.target != activeHideMenu[i].childNodes[i] && e.target != hideMenu[i].childNodes[i]){
+                hideMenu[i].classList.remove('active');
+            }
+        });
     }
 
     if (document.body.clientWidth < 780) {
@@ -79,35 +79,56 @@ document.addEventListener('DOMContentLoaded', function() {
         const weBuyItem = document.querySelectorAll('.webuy-item');
         const lastbuysItem = document.querySelectorAll('.last-buys-item');
         const wrapper = document.querySelectorAll('.swiper-wrapper');
-        weBuyItem.forEach(element => {
-            element.classList.add('swiper-slide');
-            document.querySelector('.swiper-wrapper2').append(element);  
-        });
-        trustItem.forEach(element => {
-            element.classList.add('swiper-slide');
-            document.querySelector('.swiper-wrapper1').append(element);  
-        });
-        lastbuysItem.forEach(element => {
-            element.classList.add('swiper-slide');
-            document.querySelector('.swiper-wrapper3').append(element);  
-        });
+        const homeForm = document.querySelector('.home-form');
+        const homeFormModal = document.createElement('div');
+        const homeBtnPlace = document.querySelector('.home .container div DIV');
+        const homeFormModalBtn = document.createElement('button');
+
+        homeFormModalBtn.classList.add('home-form__btn', 'btn-main');
+        homeFormModalBtn.textContent = 'Отправить заявку на оценку';
+
+        homeFormModal.classList.add('home-form__modal');
+
+        sliderMob(weBuyItem, '.swiper-wrapper2');
+        sliderMob(trustItem, '.swiper-wrapper1');
+        sliderMob(lastbuysItem, '.swiper-wrapper3');
+
         const mySwiper = new Swiper('.swiper-slider', {
             speed: 400,
             spaceBetween: 15,
             resistanceRatio: 1,
             loop: true,
             slidesPerView: 1,
+            effect: 'fade',
+            fadeEffect: {
+              crossFade: true
+            },
             autoplay: {
                 delay: 2000,
-              },
-            fadeEffect: {
-                crossFade: true,
             },
             navigation: {
                 nextEl: '.button-next',
                 prevEl: '.button-prev',
             },
         });
+
+        homeBtnPlace.append(homeFormModalBtn);
+        homeFormModal.append(homeForm);
+        document.body.append(homeFormModal);
+
+        homeFormModalBtn.addEventListener('click', ()=> {
+            homeFormModal.classList.toggle('active');
+            document.body.classList.toggle('lock');
+        }); 
+
+        homeFormModal.addEventListener('click', (e)=> {
+            if (e.target === homeFormModal) {
+                homeFormModal.classList.toggle('active');
+            }
+            document.body.classList.toggle('lock');
+        });
+
+
     }
 
 });
