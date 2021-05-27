8e9fe768eb95099f9d7ecfd8253060e2dc5c49f3
John Doe <stanislav_2010@icloud.com>
2021-05-27 17:53:13 +0500

    New modal Form Hotfix

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Main Menu
     
-    let menu = document.querySelector('.burger-menu');
-    let menuBody = document.querySelector('.header-menu');
+    const menu = document.querySelector('.burger-menu');
+    const menuBody = document.querySelector('.header-menu');
+    const group = [menu, menuBody];
+    const trustItem = document.querySelectorAll('.trust-item'),
+        weBuyItem = document.querySelectorAll('.webuy-item'),
+        lastbuysItem = document.querySelectorAll('.last-buys-item'),
+        wrapper = document.querySelectorAll('.swiper-wrapper'),
+        homeForm = document.querySelector('.home-form'),
+        homeFormModal = document.querySelector('.home-form__modal'),
+        homeFormModalContent = document.querySelector('.home-form__modal-content'),
+        homeBtnPlace = document.querySelector('.home .container div div'),
+        homeFormModalBtn = document.createElement('button');
+
+    function mainMenuToggle (){
+        group.forEach(element => {
+            element.classList.toggle('active');
+        });
+        
+        document.body.classList.toggle('lock');
+    }
+    
+    function responsive (width) {
+        if (document.body.clientWidth < width) {
+            return true;
+        }
+    }
+
+    if (menu != null) {
+        menu.addEventListener('click', function() {
+            mainMenuToggle();
+        });
+    }
 
     function sliderMob(sliderName, sliderWrapper) {
         sliderName.forEach(element => {
@@ -17,32 +49,12 @@ document.addEventListener('DOMContentLoaded', function() {
         return menuItem;
     }
 
-    if (menu != null) {
-        menu.addEventListener('click', function() {
-        let group = [menu, menuBody];
-
-        group.forEach(element => {
-            element.classList.toggle('active');
-        });
-        
+    function mobModalToggle () {
+        homeFormModal.classList.toggle('active');
         document.body.classList.toggle('lock');
-    });
     }
 
-    function ibg() {
-
-        let ibg=document.querySelectorAll(".ibg");
-
-        for (var i = 0; i < ibg.length; i++) {
-            if(ibg[i].querySelector('.ibg-src')){
-                ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-            }
-        }
-    }
-
-    ibg();
-
-    if (document.body.clientWidth < 1001) {
+    if (responsive(1001)) {
         const menu = document.querySelector('.header-menu');
         const menuList = document.querySelector('.header-menu__list');
         const phone = document.querySelector('.header-contacts__phone');
@@ -51,11 +63,13 @@ document.addEventListener('DOMContentLoaded', function() {
         
         
     } 
-    if (document.body.clientWidth < 500) {
+    if (responsive(500)) {
         const menuList = document.querySelector('.header-menu__list');
         const btn = document.querySelector('.header-contacts__btn');
         menuList.append(menuItemRend(btn));
     }
+
+    // Tabs
 
     const activeHideMenu = document.querySelectorAll('.spoiler');
     const hideMenu = document.querySelectorAll('.questions-item');
@@ -74,20 +88,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    if (document.body.clientWidth < 780) {
-        const trustItem = document.querySelectorAll('.trust-item');
-        const weBuyItem = document.querySelectorAll('.webuy-item');
-        const lastbuysItem = document.querySelectorAll('.last-buys-item');
-        const wrapper = document.querySelectorAll('.swiper-wrapper');
-        const homeForm = document.querySelector('.home-form');
-        const homeFormModal = document.createElement('div');
-        const homeBtnPlace = document.querySelector('.home .container div DIV');
-        const homeFormModalBtn = document.createElement('button');
+    if (responsive(780)) {
 
-        homeFormModalBtn.classList.add('home-form__btn', 'btn-main');
+        homeFormModalBtn.classList.add('home-form__btn', 'btn-main', 'btn-modal');
         homeFormModalBtn.textContent = 'Отправить заявку на оценку';
 
-        homeFormModal.classList.add('home-form__modal');
+        homeBtnPlace.append(homeFormModalBtn);
+        homeFormModalContent.append(homeForm);
+        document.body.append(homeFormModal);
+
+        
+        document.querySelectorAll('.btn-modal').forEach(element => {
+            element.addEventListener('click', ()=> {
+                mobModalToggle();
+            });
+        });
+
+        homeFormModal.addEventListener('click', (e)=> {
+            if (e.target === homeFormModal) {
+                mobModalToggle();
+            }
+        });
+
 
         sliderMob(weBuyItem, '.swiper-wrapper2');
         sliderMob(trustItem, '.swiper-wrapper1');
@@ -110,22 +132,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 nextEl: '.button-next',
                 prevEl: '.button-prev',
             },
-        });
-
-        homeBtnPlace.append(homeFormModalBtn);
-        homeFormModal.append(homeForm);
-        document.body.append(homeFormModal);
-
-        homeFormModalBtn.addEventListener('click', ()=> {
-            homeFormModal.classList.toggle('active');
-            document.body.classList.toggle('lock');
-        }); 
-
-        homeFormModal.addEventListener('click', (e)=> {
-            if (e.target === homeFormModal) {
-                homeFormModal.classList.toggle('active');
-            }
-            document.body.classList.toggle('lock');
         });
 
 
